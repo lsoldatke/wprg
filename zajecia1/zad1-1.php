166ae396451c7a1c51b9e09b30db288901b4f3c7
s24729-pj <s24729@pjwstk.edu.pl>
2023-03-15 12:48:40 +0100

    Dodanie zadań 1.1, 1.2

---
+++ zajecia1/zad1-1.php
@@ -0,0 +1,6 @@
+<?php
+function throw_a_dice() {
+    return rand(1, 6);
+}
+
+echo throw_a_dice();
---
+++ zajecia1/zad1-2.php
@@ -0,0 +1,6 @@
+<?php
+function calculate_circle_diameter($radius) {
+    return 2 * $radius;
+}
+
+echo calculate_circle_diameter(2);
