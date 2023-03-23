84964481d6417e44a20a2671465d48fbd79859c1
s24729-pj <s24729@pjwstk.edu.pl>
2023-03-23 10:43:04 +0100

    Dodanie zadania 3.2

---
+++ zajecia1/zad3-2.php
@@ -0,0 +1,12 @@
+<?php
+function throw_a_dice_n_times($n) {
+    $results = [];
+    for ($i = 0; $i < $n; $i++) {
+        array_push($results, rand(1, 6));
+    }
+    return $results;
+}
+
+foreach (throw_a_dice_n_times(5) as $result) {
+    echo $result." ";
+}
