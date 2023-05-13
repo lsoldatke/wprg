1ac1ae17a6b086d03b1d98c8d433d0ffe6e259b0
s24729 <s24729@pjwstk.edu.pl>
2023-05-13 16:41:15 +0200

    Modyfikacja zadania z zajec 5

--- zajecia5/index.php
+++ zajecia5/index.php
@@ -1,10 +1,12 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pl">
 <head>
     <meta charset="UTF-8">
     <title>Portal z samochodami</title>
 </head>
 <body>
+    <form>
 
+    </form>
 </body>
 </html>
