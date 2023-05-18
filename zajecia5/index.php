65b2f25523c5115dc01f7b83ed76dc2a0c04cc27
s24729-pj <s24729@pjwstk.edu.pl>
2023-05-18 17:01:18 +0200

    Modyfikacja zadania z zajec 5

--- zajecia5/index.php
+++ zajecia5/index.php
@@ -5,8 +5,19 @@
     <title>Portal z samochodami</title>
 </head>
 <body>
-    <form>
+    <table>
+        <tr>
+            <td><a href="index.php">Strona glowna</a></td>
+            <td><a href="allcars.php">Wszystkie samochody</a></td>
+            <td><a href="addcar.php">Dodaj samochod</a></td>
+        </tr>
+    </table>
+    <?php
+    echo '<table>';
+    echo '<tr>';
 
-    </form>
+    echo '</tr>';
+    echo '</table>';
+    ?>
 </body>
 </html>
